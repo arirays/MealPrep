2278a0d26c0a1a413e4a9ac6645b20a0650da2cc
arirays <arishhu@gmail.com>
2023-07-25 20:38:06 -1000

    Added new sheet view to add new recipes

---
+++ MealPrep/Views/ExploreRecipes/RecipesListView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  MealPrep
+//
+//  Created by Ari on 10/2/22.
+//
+
+import SwiftUI
+
+struct RecipesListView: View {
+    
+    @EnvironmentObject var recipeData: RecipeData
+    let category: MainInformation.Category
+    
+    @State private var isPresenting = false
+    @State private var newRecipe = Recipe()
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+            }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle("All recipes")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    isPresenting = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .sheet(isPresented: $isPresenting) {
+            NavigationView {
+                ModifyRecipeView(recipe: $newRecipe)
+                    .toolbar(content: {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            if newRecipe.isValid {
+                                Button("Add") {
+                                    recipeData.add(recipe: newRecipe)
+                                    isPresenting = false
+                                }
+                            }
+                        }
+                    })
+                    .navigationTitle("Add a New Recipe")
+            }
+        }
+    }
+}
+
+extension RecipesListView {
+    
+    private var recipes: [Recipe] {
+        recipeData.recipes(for: category)
+    }
+    
+    private var navigationTitle: String {
+        "\(category.rawValue) Recipes"
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RecipesListView(category: .breakfast)
+                .environmentObject(RecipeData())
+        }
+    }
+}
