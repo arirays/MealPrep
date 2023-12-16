7d0b619d315d044aaf9f49aec0b7ddefedd8e0ab
arirays <arishhu@gmail.com>
2023-12-15 15:20:57 -1000

    created new views

--- MealPrep/Views/ModifyRecipes/ModifyRecipeView.swift
+++ MealPrep/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -25,7 +25,7 @@ struct ModifyRecipeView: View {
             case .main:
                 ModifyMainInformationView(mainInformation: $recipe.mainInformation)
             case .ingredients:
-                Text("Ingredients Editor")
+                ModifyIngredientsListView(ingredients: $recipe.ingredients)
             case .directions:
                 Text("Directions Editor")
             }
